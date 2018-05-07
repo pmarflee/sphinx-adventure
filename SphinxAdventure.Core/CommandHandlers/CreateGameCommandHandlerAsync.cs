1cdb63ddbf3289169e763a653056ff6221dea9ff
Paul Marfleet <paul.marfleet@gmail.com>
2018-05-07 23:16:02 +0100

    Removed user lookup when creating a new game

--- SphinxAdventure.Core/CommandHandlers/CreateGameCommandHandlerAsync.cs
+++ SphinxAdventure.Core/CommandHandlers/CreateGameCommandHandlerAsync.cs
@@ -11,31 +11,20 @@ namespace SphinxAdventure.Core.CommandHandlers
     public class CreateGameCommandHandlerAsync : RequestHandlerAsync<CreateGameCommand>
     {
         private readonly IRepository<Game> _gameRepository;
-        private readonly IRepository<User> _userRepository;
 
-        public CreateGameCommandHandlerAsync(
-            IRepository<Game> gameRepository,
-            IRepository<User> userRepository)
+        public CreateGameCommandHandlerAsync(IRepository<Game> gameRepository)
         {
             _gameRepository = gameRepository;
-            _userRepository = userRepository;
         }
 
         public override async Task<CreateGameCommand> HandleAsync(
             CreateGameCommand command, 
             CancellationToken cancellationToken = default(CancellationToken))
         {
-            var user = await _userRepository.GetAsync(command.UserId);
-
-            if (user == null)
-            {
-                throw new Exception("User not found");
-            }
-
             var game = new Game
             {
                 Id = command.Id,
-                UserId = user.Id,
+                UserId = command.UserId,
                 CreatedOn = command.CreatedOn
             };
 
